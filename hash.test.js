bc7b043f36041f998102bca92c2cceca310c58b0
Anna Baik <annaheyonbaik@gmail.com>
2020-10-11 10:52:03 +0100

    Improved test name

--- hash.test.js
+++ hash.test.js
@@ -1,6 +1,6 @@
 const Hash = require("./hash.js");
 
-it('returns a string', () =>
+it('returns a string hashed with SHA256', () =>
   expect(Hash.HashPassword("123")).toBe(
     "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3")
 )
